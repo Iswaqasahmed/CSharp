8e03f4452671f7387c6509f3fd0cb2ec55ccd570
waqas ahmed <waqasahmeddeveloper@gmail.com>
2023-05-13 14:07:38 +0500

    Added Access Modifier

---
+++ AccessModifier/AccessDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Constructor
+{
+    class AccessDemo
+    {
+
+        private int? universityId { get; set; }
+        public string universityName { get;  set; }
+
+        protected int universityDepartRanking { get; set; }
+
+
+
+    }
+}
---
+++ OOP/Constructor/ConA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Constructor
+{
+    class ConA
+    {
+        // constructor ?
+        //public ConA()
+        //{
+
+        //}
+
+        static ConA()
+        {
+
+        }
+        
+    }
+
+       
+    
+
+
+}
