424c08c5ad981f4b6911de4c6bd62094bac169e5
= <=>
2023-05-27 19:44:20 +0500

    added

---
+++ String/ConString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharp.String
+{
+    public class ConString // string concept
+    {
+
+
+        public void DisplayString()
+        {
+            string str1 = "new string1";
+            string str2 = "new string2";
+            str1 += str1 + " ";
+            Console.WriteLine(str1, str2);
+        }
+
+
+    }
+}
